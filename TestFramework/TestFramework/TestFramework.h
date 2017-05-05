d70c872cbc6ae36bf93a5c40dc49765e249ff50a
analysys <zhiku@analysys.com.cn>
2017-05-05 18:32:08 +0800

    增加demo文件

---
+++ TestFramework/TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by analysys on 2017/5/5.
+//  Copyright © 2017年 analysys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
